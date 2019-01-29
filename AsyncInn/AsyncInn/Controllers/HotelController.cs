77cafdd189a7eff0109250b046c65aa300b35365
Deliman206 <smiller206@me.com>
2019-01-28 23:13:11 -0800

    added injection for Hotel, has bugs not sure what to do

--- AsyncInn/AsyncInn/Controllers/HotelController.cs
+++ AsyncInn/AsyncInn/Controllers/HotelController.cs
@@ -6,14 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 using AsyncInn.Models;
 using AsyncInn.Data;
 using Microsoft.EntityFrameworkCore;
+using AsyncInn.Models.Interfaces;
+using AsyncInn.Models.ViewModels;
 
 namespace AsyncInn.Controllers
 {
     public class HotelController : Controller
     {
+        private readonly IHotelManeger _context;
+
+        public HotelController(IHotelManeger context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Create(HotelCreateViewModel hotel2)
+        {
+            Hotel newHotel = hotel2.Hotel;
+            if (ModelState.IsValid)
+            {
+                await _context.CreateHotel(newHotel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(newHotel);
         }
     }
 }
---
+++ AsyncInn/AsyncInn/Models/ViewModels/HotelCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.ViewModels
+{
+    public class HotelCreateViewModel
+    {
+        public Hotel Hotel { get; set; }
+    }
+}
